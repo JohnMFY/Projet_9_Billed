ac9cdd5f9dc4c1cf1de4388229924f721b96c6b2
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-01-29 16:23:28 +0100

    Fix test 404 et 500 TODO

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -14,6 +14,7 @@ import userEvent from "@testing-library/user-event";
 import Store from "../app/Store.js";
 import store from "../__mocks__/store.js";
 
+
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
@@ -187,6 +188,55 @@ describe('Given I am a user connected as Employee', () => {
     
       expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["NewBill"]);
 
+    })  
+  })
+
+  describe("Erreur API", () => {
+    beforeEach(() => {
+      jest.spyOn(store, "bills")
+      Object.defineProperty(
+          window,
+          'localStorage',
+          { value: localStorageMock }
+      )
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+        email: "a@a"
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+    })
+    test("fetches bills from an API and fails with 404 message error", async () => {
+
+      store.bills.mockImplementationOnce(() => {
+        return {
+          list : () =>  {
+            return Promise.reject(new Error("Erreur 404"))
+          }
+        }})
+      window.onNavigate(ROUTES_PATH.Bills)
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+
+    test("fetches messages from an API and fails with 500 message error", async () => {
+
+      store.bills.mockImplementationOnce(() => {
+        return {
+          list : () =>  {
+            return Promise.reject(new Error("Erreur 500"))
+          }
+        }})
+
+      window.onNavigate(ROUTES_PATH.Bills)
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
     })
   })
+ 
 })
+
